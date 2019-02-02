81e74b6e1d0ddddfd1163e87a15d9826da63e822
Ashanti Haring <ashanti.haring@gmail.com>
2019-02-02 14:58:59 +0100

    Toggle Uploader Modal

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ export default class App extends React.Component {
     }
     showUploader() {
         this.setState({
-            uploaderIsVisible: true
+            uploaderIsVisible: !this.state.uploaderIsVisible
         });
     }
 
