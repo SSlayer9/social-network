9a25d8373753dbfc58ec6aa7ebd4d675e4280f78
Ashanti Haring <ashanti.haring@gmail.com>
2019-02-06 15:07:05 +0100

    Testing

--- src/testing/profile.test.js
+++ src/testing/profile.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import App from "./app";
-import Profile from "../profile";
-import Profilepic from "./ProfilePic";
-import BioEditor from "./BioEditor";
+import App from "../app";
+import Profile from "../Profile";
+import Profilepic from "../ProfilePic";
+import BioEditor from "../BioEditor";
 import { shallow } from "enzyme";
 
 // When no bio is passed to it, an "Add" button is rendered.
@@ -22,3 +22,22 @@ test("bio is there, edit button is rendered", () => {
 });
 
 // Clicking either the "Add" or "Edit" button causes a textarea and a "Save" button to be rendered.
+
+test("clicking add bio button showstextarea and save button", () => {
+    const wrapper = shallow(<p
+        onClick={props.toggleBioEditor}
+        className="to-editor-link"> Add Your Bio Now</p>);
+
+    wrapper.find("p").simulate("click");
+    const wrapper2 = shallow(<Profile />)
+    expect(wrapper2.)
+});
+
+// ---------
+it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+});
+});
