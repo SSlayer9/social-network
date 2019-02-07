c1d71b83b630a625bdf6e72ff6c0b1cf3973cbc8
Ashanti Haring <ashanti.haring@gmail.com>
2019-02-07 18:17:35 +0100

    Part7

--- src/friends.js
+++ src/friends.js
@@ -11,19 +11,40 @@ class Friends extends React.Component {
         this.props.dispatch(receiveFriendsAndWannabes());
     }
 
+    TODO: // chekc this render function!!!!!!!!!!! NOT WORKING!
     render() {
-        return <h1>Hi, I am the friends page!</h1>;
-    }
+        return  (
+
+     const wannabeList = wannabes.map((wannabe => {
+         <li key={wannabe.id}>{wannabe.first} {wannabe.last}</li>
+             <img src={wannabe.url} />
+             <button onCLick={e => this.props.dispatch(acceptFriendship(wannabe.id))}> Accept</button>
+        }))
+
+        const friends = friends.map((friend => {
+                 <div key={friend.id}>
+                     <img src={friend.url}></img>
+                     <p> {friend.first} {friend.last}</p>
+                     <button onClick={e => this.props.dispatch(endFriendship(friend.id))}></button>
+                 </div>
+             }))
+
+        )
+    
+        
+        
+    <p>Hi, I am the friends page!</p>;
+    
 }
 
 const mapStateToProps = function(state) {
     console.log("STATE:", state);
     return {
-        OpenFriendsrequest:
+        wannabes:
             state.friendslist &&
             state.friendslist.filter(friend => friend.accepted == false),
 
-        acceptedFriendrequest:
+        friends:
             state.friendslist &&
             state.friendslist.filter(friend => friend.accepted == true)
     };
